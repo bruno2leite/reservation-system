5a1f33d47fc736fce25139ec256328a0f4249c5e
bruno <134561203+bruno2leite@users.noreply.github.com>
2025-07-02 13:42:45 -0300

    Update ReservationResponseDTO.java

--- src/main/java/com/brunoleite/reservation/DTO/ReservationResponseDTO.java
+++ src/main/java/com/brunoleite/reservation/DTO/ReservationResponseDTO.java
@@ -1,9 +1,9 @@
 package com.brunoleite.reservation.DTO;
 
 public record ReservationResponseDTO(String nameTable,
-									 String nameUser,
-									 int tableCapacity,
-									 String reservationTime,
-									 String reservationId) {
-
+				     String nameUser,
+				     int tableCapacity,
+				     String reservationTime,
+				     String reservationId) {
+	
 }
